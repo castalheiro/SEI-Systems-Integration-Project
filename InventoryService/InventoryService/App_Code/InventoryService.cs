c870174080c158f9da0d7ae36042c6be35e76610
Alberto Filipe Carvalho <lusoafc@gmail.com>
2015-04-30 19:48:58 +0100

    [ENH] Production orchestration and first draft of Inventory Web Service

---
+++ InventoryService/InventoryService/App_Code/InventoryService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for InventoryService
+/// </summary>
+[WebService(Namespace = "http://InventoryService/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class InventoryService : System.Web.Services.WebService {
+
+    public InventoryService () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public void DecreaseStock(string category, string option)
+    {
+        //TODO
+
+        while (true) { DecreaseStock("a", "b"); }
+    }
+    
+}
